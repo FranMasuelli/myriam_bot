b3514edb2755ebda8c3c0b8155583633caf16140
FranMasuelli <89875724+FranMasuelli@users.noreply.github.com>
2023-10-01 21:27:19 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+
+while True:
+    p = sync_playwright().start()
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://viz.flowics.com/public/7cdb790cceb696f76ec147008429eed8/650b2cf01b9b89db3a006742/live")
+    vote_selector = page.locator(".answers .answer").all()[4]
+    vote_selector.click()
+    page.close()
+    p.stop()
+
+
+
+
