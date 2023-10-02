c5c326e106fc244fc45833fbf5ee132fb2a5015c
FranMasuelli <89875724+FranMasuelli@users.noreply.github.com>
2023-10-01 21:30:36 -0300

    Add Readme and headless

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 while True:
     p = sync_playwright().start()
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch()
     page = browser.new_page()
 
     page.goto("https://viz.flowics.com/public/7cdb790cceb696f76ec147008429eed8/650b2cf01b9b89db3a006742/live")
