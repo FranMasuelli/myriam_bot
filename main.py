4904ad296e2b2b85d66c356a46a8cf7b6a53aa1d
FranMasuelli <89875724+FranMasuelli@users.noreply.github.com>
2023-10-01 22:17:46 -0300

    Clear local storage

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from playwright.sync_api import sync_playwright
+import time
+
+p = sync_playwright().start()
+browser = p.chromium.launch(headless=False)
+page = browser.new_page()
 
 while True:
-    p = sync_playwright().start()
-    browser = p.chromium.launch()
-    page = browser.new_page()
-
     page.goto("https://viz.flowics.com/public/7cdb790cceb696f76ec147008429eed8/650b2cf01b9b89db3a006742/live")
     vote_selector = page.locator(".answers .answer").all()[4]
     vote_selector.click()
-    page.close()
-    p.stop()
+    page.evaluate("() => window.localStorage.clear()")
+    page.evaluate("() => window.sessionStorage.clear()")
+    time.sleep(1)
+
+    
 
 
 
